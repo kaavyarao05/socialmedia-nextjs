09fc1e2f3cd1702043c3489727ef9bff50b0b31c
kaavya <kaavyarao05@gmail.com>
2024-07-14 01:14:57 +0530

    feat: added footer to root page.jsx and Profile/[account]/page.jsx fix: header now sticks to the top of the webpage fix: the background color now fits to the entire webpage

--- app/Profile/[Account]/page.jsx
+++ app/Profile/[Account]/page.jsx
@@ -1,16 +1,18 @@
 "use client";
 import { usePathname } from "next/navigation";
 
+
+
 function header(){
-    const pathname=usePathname();
+    const username=usePathname().split('/')[2];
     return(
       <>
-      <header class='w-full max-h-28 px-10 pb-8 mx-auto rounded-b-lg bg-white shadow-xl'>
+      <header class='w-full max-h-28 px-10 pb-8 mx-auto rounded-b-lg bg-white shadow-md'>
           <div class='text-base pt-4 leading-3 background bg-white mx-auto space-y-6'>
             <button onClick={() => window.location.href="../../.."} class="bg-transparent hover:bg-orange-400 text-emerald-400 font-semibold hover:text-white py-2 px-4 border border-emerald-400 hover:border-transparent rounded">
             Go Home
             </button>
-            <h1 class="flex items-center justify-center text-2xl font-bold text-black">{pathname.split('/')[2]}'s Profile</h1>
+            <h1 class="flex items-center justify-center text-2xl font-bold text-black">{username}'s Profile</h1>
             
           </div>
       </header>
@@ -19,12 +21,29 @@ function header(){
     )
   }
 
+function footer(){
+  const username=usePathname().split('/')[2];
+  return(
+    <>
+    <footer class='fixed bottom-0 w-full max-h-24 px-10 py-3 pb-5 mx-auto rounded-t-lg bg-white shadow-xl'>
+        <div class='leading-3 mx-auto space-y-3'>
+          <h1 class="text-lg font-bold text-black">You are currently visiting {username}'s profile</h1>
+        </div>
+    </footer>
+    </>
+  )
+}
+
 export default function Post() {
 const pathname = usePathname().split()[2];
 return (
-    <>
+    <div class='min-h-screen from-emerald-400 via-amber-300 to-orange-400 bg-gradient-to-r'>
     {header()}
-    </>
+    <div class=''>
+      <h2 class="text-center py-5 text-black">This user has not posted anything</h2>
+    </div>
+    {footer()}
+    </div>
 ); 
 
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 'use client';
 import React from 'react'
 
-const user1="User1"
 const text1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet libero aliquet, tristique mauris at, semper lorem. Nunc efficitur pulvinar massa, id imperdiet odio pulvinar ut. Curabitur mattis orci eu magna imperdiet tincidunt. Aliquam nibh mauris, iaculis sit amet blandit id, luctus in est. Ut vitae turpis diam."
 
-const user2="User2"
 const text2="Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla sit amet neque et augue mattis pellentesque eget ac augue. Phasellus ac nunc venenatis, ullamcorper nunc eu, dictum diam. Duis eleifend, metus ac pharetra consequat, sapien diam dictum tortor, et ultricies nunc tellus id dolor. Aliquam accumsan porta pellentesque. Proin quis nibh arcu."
 
 const changePage = (url)=>{
@@ -14,15 +12,26 @@ const changePage = (url)=>{
 function header(){
   return(
     <>
-    <header class='sticky top-0 w-full max-h-24 px-10 py-3 pb-5 mx-auto rounded-b-lg bg-white shadow-xl'>
+    <header class='sticky top-0 w-full max-h-24 px-10 py-3 pb-5 mx-auto rounded-b-lg bg-white shadow-md'>
         <div class='leading-3 mx-auto space-y-3'>
           <h1 class="text-2xl font-bold text-black">Home Page</h1>
           <button onClick={() => {changePage("/about")}} class="bg-transparent hover:bg-orange-400 text-emerald-400 font-semibold hover:text-white py-2 px-4 border border-emerald-400 hover:border-transparent rounded">
           About
           </button>
-          <input type='text' class="border-orange-400"></input>
         </div>
     </header>
+    </>
+  )
+}
+
+function footer(){
+  return(
+    <>
+    <footer class='fixed bottom-0 w-full max-h-24 px-10 py-3 pb-5 mx-auto rounded-t-lg bg-white drop-shadow-xl'>
+        <div class='leading-3 mx-auto space-y-3'>
+          <h1 class="text-lg font-bold text-black">Read other users posts here</h1>
+        </div>
+    </footer>
     </>
   )
 }
@@ -30,12 +39,12 @@ function header(){
 function userPost(user,text){
   return(
     <>
-    <div class="flex m-auto my-3 pt-5 px-3 max-w-3xl bg-white rounded">
+    <div class="flex flex-col m-auto my-3 py-5 px-20 max-w-3xl bg-white rounded shadow hover:shadow-md">
       <div class>
-        <button onClick={()=>{changePage("Profile/"+user)}} id="username" class="text-black font-semibold min-w-fit">{user} posted</button>
+        <button onClick={()=>{changePage("Profile/"+user)}} id="username" class=" hover:text-orange-400 text-emerald-400 font-semibold py-2 leading-10">{user}</button>
       </div>
       <div>
-        <p class="text-black font-medium py-14">{text}</p>
+        <p class="text-black font-medium break-normal">{text}</p>
       </div>
     </div>
     </>
@@ -44,13 +53,16 @@ function userPost(user,text){
 
 const page = () => {
   return (
-    <>
+    <div class='min-h-screen from-emerald-400 via-amber-300 to-orange-400 bg-gradient-to-r'>
     {header()}
-    <div class='content-center'>
-    {userPost(user1,text1)}
-    {userPost(user2,text2)}
+    <div class=''>
+    {userPost("User1",text1)}
+    {userPost("User2",text2)}
+    {userPost("User3",text1)}
+    {userPost("User4",text2)}
     </div>
-    </>
+    {footer()}
+    </div>
   )
 }
 
