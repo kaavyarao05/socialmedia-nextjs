b53e8defef986b41f3b5706f6bbb72eb13a15873
kaavya <kaavyarao05@gmail.com>
2024-08-02 17:51:29 +0530

    feat: used Image component for logo and profile image in header, links in footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'node_modules/next/link';
+import Image from 'node_modules/next/image';
 
-function card(link,text){
+import Github from '/public/github.png';
+import Leetcode from '/public/leetcode.webp';
+import Linkedin from '/public/linkedin.webp';
+
+function card(link,image,text){
     return(
        <Link href={link}>
-        <div className='flex text-center min-h-20 min-w-40 mb-10 mt-4 py-5 px-2 rounded-md shadow-lg hover:shadow-md flex-col bg-white text-black'>        
-                <div>Image</div>
+        <div className='flex text-center min-h-20 min-w-60 mb-10 mt-4 py-5 px-2 rounded-md shadow-lg hover:shadow-md flex-col bg-white text-black'>        
+                <Image src={Leetcode} height={50} width={50} className='mx-auto my-2'/>
                 <div>{text}</div>       
         </div>
         </Link>
@@ -16,9 +21,24 @@ const Footer = () => {
   return (
     <footer className="bottom-0 flex items-center from-emerald-200 via-amber-100 to-orange-200 bg-gradient-to-r">
         <div className="flex flex-row space-x-10 mx-auto my-10 max-w-fit px-2">
-            {card("https://github.com/kaavyarao05","GitHub")}
-            {card("https://www.linkedin.com/in/kaavya-rao-33a7752b7/","LinkedIn")}
-            {card("https://leetcode.com/u/kaavyarao05/","LeetCode")}
+        <Link href={"https://github.com/kaavyarao05"}>
+        <div className='flex text-center min-h-20 min-w-60 mb-10 mt-4 py-5 px-2 rounded-md shadow-lg hover:shadow-md flex-col bg-white text-black'>        
+                <Image src={Github} height={50} width={50} className='mx-auto my-2'/>
+                <div>GitHub</div>       
+        </div>
+        </Link>
+        <Link href={'https://www.linkedin.com/in/kaavya-rao-33a7752b7/'}>
+        <div className='flex text-center min-h-20 min-w-60 mb-10 mt-4 py-5 px-2 rounded-md shadow-lg hover:shadow-md flex-col bg-white text-black'>        
+                <Image src={Linkedin} height={50} width={50} className='mx-auto my-2'/>
+                <div>LinkedIn</div>       
+        </div>
+        </Link>
+        <Link href={"https://leetcode.com/u/kaavyarao05/"}>
+        <div className='flex text-center min-h-20 min-w-60 mb-10 mt-4 py-5 px-2 rounded-md shadow-lg hover:shadow-md flex-col bg-white text-black'>        
+                <Image src={Leetcode} height={50} width={50} className='mx-auto my-2'/>
+                <div>LeetCode</div>       
+        </div>
+        </Link>
         </div>
     </footer>
   )
--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,8 +2,10 @@
 import React from 'react';
 import About from './AboutButton';
 import Home from './HomeButton';
-
-var profile_image_link="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+import Image from 'node_modules/next/image';
+import ProfileImage from 'public/profileimage.jpg'
+import Link from 'node_modules/next/link';
+import Logo from 'public/logo.png';
 
 const changePage = (url)=>{
     window.location.href = url;
@@ -12,38 +14,25 @@ const changePage = (url)=>{
 const NavBar = () => {
   return (
     <div>
-        <nav class="from-emerald-200 via-amber-100 to-orange-200 bg-gradient-to-r">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
+        <nav className="from-emerald-200 via-amber-100 to-orange-200 bg-gradient-to-r">
+  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="relative flex h-16 items-center justify-between">
+      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div className="hidden sm:ml-6 sm:block">
+          <div className="flex space-x-4 py-5">
+            <Image src={Logo} width={65} height={40} className='mr-10 '></Image>
             <About></About>
             <Home></Home>
           </div>
         </div>
       </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
         
-        <div class="relative ml-3">
+        <div className="relative ml-3">
           <div>
-            <button type="button" onClick={()=>{changePage("Profile/User/")}} class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">Open user menu</span>
-              <img class="h-8 w-8 rounded-full" src={profile_image_link} alt=""/>
-            </button>
+            <Link href={"/Profile/User/"}>
+              <Image src={ProfileImage} height={40} width={40} className='rounded-full border-transparent hover:border-white border-x-4 border-y-4'/>
+            </Link>
           </div>
         </div>
       </div>
